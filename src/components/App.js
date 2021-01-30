580e7bc019aa79de42f546fd8795c8709182bf21
jyliu17 <jyliu17@gmail.com>
2021-01-29 22:40:05 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -16,11 +16,22 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
+
+
+
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
+      <Home />
+      <About />
       {/* write an <About> component here */}
     </div>
   );
